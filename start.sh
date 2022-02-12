5e26d6944a1cecf1f5727722c545f2fc7130e3c6
Brian Redmond <chzbrgr71@users.noreply.github.com>
2022-02-12 21:37:50 +0000

    fixed jmeter

--- start.sh
+++ start.sh
@@ -5,8 +5,9 @@ mkdir -p outputs
 export PREFIX='briar'
 export SUFFIX=$RANDOM
 export RG=$PREFIX-cont-app-reddog-$SUFFIX
-#export LOCATION='canadacentral'
 export LOCATION='eastus'
+#export RG=$PREFIX-cont-app-reddog-canada
+#export LOCATION='canadacentral'
 export LOGFILE_NAME="./outputs/${RG}.log"
 
 ./walk-the-dog.sh $RG $LOCATION $SUFFIX 2>&1 | tee -a $LOGFILE_NAME
